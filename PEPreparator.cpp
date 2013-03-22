3044e28f700270e9bb6dbb0baead0829bf17a809
ramntry <ramntry@gmail.com>
2013-03-22 20:22:41 +0400

    image loading is done for now

--- PEPreparator.cpp
+++ PEPreparator.cpp
@@ -18,21 +18,131 @@ PEPreparator::DOSHeader::DOSHeader()
     std::memset(this, 0, size);
 }
 
+PEPreparator::PEHeader::PEHeader()
+{
+    assert(sizeof(*this) == size());
+    std::memset(this, 0, size());
+}
+
+PEPreparator::SectionHeader::SectionHeader()
+{
+    assert(sizeof(*this) == size);
+    std::memset(this, 0, size);
+}
+
+int PEPreparator::PEHeader::size() const
+{
+    return mMagicNumberSize + fst.size + snd.size;
+}
+
 bool PEPreparator::DOSHeader::magicNumberIsCorrect() const
 {
+    assert(mMagicNumberSize == sizeof(mMagicNumber));
+    assert(mMagicNumberSize == 2);
+
     return mMagicNumber[0] == 'M'
         && mMagicNumber[1] == 'Z';
 }
 
-bool PEPreparator::loadDOSHeader()
+bool PEPreparator::PEHeader::magicNumberIsCorrect() const
+{
+    assert(mMagicNumberSize == sizeof(mMagicNumber));
+    assert(mMagicNumberSize == 4);
+
+    return mMagicNumber[0] == 'P'
+        && mMagicNumber[1] == 'E'
+        && mMagicNumber[2] == '\0'
+        && mMagicNumber[3] == '\0';
+}
+
+bool PEPreparator::loadFromFile(int pos, int size, char *dst, std::string const &name)
 {
     mFile.clear();
-    mFile.seekg(0);
-    mFile.read(reinterpret_cast<char *>(&mDOSHeader), DOSHeader::size);
-    if (mFile.gcount() != DOSHeader::size) {
-        error() << "Couldn't load the DOS header: only " << mFile.gcount() << " of " << DOSHeader::size
+    mFile.seekg(pos);
+    mFile.read(dst, size);
+    if (mFile.gcount() != size) {
+        error() << "Couldn't load " << name << ": only " << mFile.gcount() << " of " << size
             << " bytes was loaded" << std::endl;
         return false;
+    }
+    return true;
+}
+
+bool PEPreparator::loadDOSHeader()
+{
+    return loadFromFile(0, mDOSHeader.size, reinterpret_cast<char *>(&mDOSHeader), "the DOS header");
+}
+
+bool PEPreparator::loadPEHeader()
+{
+    if (static_cast<int>(mDOSHeader.peOffset) < mDOSHeader.size) {
+        warning() << "Loading PE header with offset lower than standard DOS header size (" << mDOSHeader.size
+            << ")" << std::endl;
+    }
+    return loadFromFile(mDOSHeader.peOffset, mPEHeader.size(), reinterpret_cast<char *>(&mPEHeader)
+        , "the PE header");
+}
+
+void PEPreparator::Image::setNumofSections(int numofSections)
+{
+    mSectionHeaders.resize(numofSections);
+    mSections.resize(numofSections);
+}
+
+int PEPreparator::Image::sectionHeadersSizeInBytes() const
+{
+    return mSectionHeaders.size() * SectionHeader::size;
+}
+
+char *PEPreparator::Image::rawSectionHeaders()
+{
+    return reinterpret_cast<char *>(&mSectionHeaders[0]);
+}
+
+PEPreparator::Image::Section &PEPreparator::Image::sectionAt(int index)
+{
+    return mSections.at(index);
+}
+
+int PEPreparator::Image::rawOffsetOfSectionAt(int index) const
+{
+    return mSectionHeaders.at(index).pointerToRawData;
+}
+
+void PEPreparator::Image::initSectionSizes()
+{
+    assert(mSections.size() == mSectionHeaders.size());
+    assert(mSections.size() != 0);
+
+    for (size_t i = 0; i < mSections.size(); ++i) {
+        mSections[i].resize(mSectionHeaders[i].virtualSize, 0);
+    }
+}
+
+std::string PEPreparator::Image::nameOfSectionAt(int index) const
+{
+    return std::string(&mSectionHeaders.at(index).name[0], SectionHeader::nameSize);
+}
+
+bool PEPreparator::loadImage()
+{
+    mImage.setNumofSections(mPEHeader.fst.numofSections);
+    int sectionHeadersOffset = mDOSHeader.peOffset + mPEHeader.size();
+    note() << "Section headers offset is " << sectionHeadersOffset << std::endl;
+
+    if (!loadFromFile(sectionHeadersOffset, mImage.sectionHeadersSizeInBytes(), mImage.rawSectionHeaders()
+            , "section headers")) {
+        return false;
+    }
+    mImage.initSectionSizes();
+
+    for (int i = 0; i < mPEHeader.fst.numofSections; ++i) {
+        if (!loadFromFile(mImage.rawOffsetOfSectionAt(i), mImage.sectionAt(i).size(), &mImage.sectionAt(i)[0]
+                , "a section")) {
+            return false;
+        }
+        note() << "Section " << mImage.nameOfSectionAt(i) << " is loaded (" << mImage.sectionAt(i).size()
+            << " bytes)" << std::endl;
     }
     return true;
 }
@@ -43,8 +153,32 @@ bool PEPreparator::checkDOSHeader()
         warning() << "MZ magic number isn't present" << std::endl;
     }
     if (mDOSHeader.peOffset >= mRawSize) {
-        warning() << "PE offset presented in DOS header is greater (or equal) then file size" << std::endl;
+        warning() << "PE offset presented in DOS header (" << mDOSHeader.peOffset
+            << ") is greater (or equal) then file size" << std::endl;
     }
+    return true;
+}
+
+bool PEPreparator::checkPEHeader()
+{
+    if (!mPEHeader.magicNumberIsCorrect()) {
+        warning() << "PE magic number isn't present" << std::endl;
+    }
+    if (mPEHeader.fst.sizeofOptionalHeader != mPEHeader.snd.size) {
+        warning() << "size of optional NT header presented in NT file header ("
+            << mPEHeader.fst.sizeofOptionalHeader << ") is not match with the standard value ("
+            << mPEHeader.snd.size << "). Still uses the standard value!" << std::endl;
+    }
+
+    size_t entryPointAddress = mPEHeader.snd.imageBase + mPEHeader.snd.entryPointRva;
+    note() << "Image base address is 0x" << std::hex << mPEHeader.snd.imageBase << std::endl;
+    note() << "Entry point RVA is 0x" << mPEHeader.snd.entryPointRva << " (0x" << entryPointAddress
+        << ")" << std::dec << std::endl;
+
+    if (entryPointAddress >= 0x80000000) {
+        warning() << "Entry point have too high address" << std::endl;
+    }
+    note() << "Number of sections: " << mPEHeader.fst.numofSections << std::endl;
     return true;
 }
 
@@ -53,14 +187,15 @@ int PEPreparator::findPEOffset()
     if (mDOSHeader.peOffset < mRawSize) {
         return mDOSHeader.peOffset;
     }
-    warning() << "try to restore PE offset" << std::endl;
+    warning() << "Try to restore PE offset" << std::endl;
     mFile.clear();
     mFile.seekg(0);
-    int result = 0;
+    Dword &result = mDOSHeader.peOffset;
+    result = 0;
     while (mFile) {
         if (mFile.get() == 'P' && mFile.get() == 'E') {
-            result = static_cast<int>(mFile.tellg()) - 2;
-            if (result >= DOSHeader::size) {
+            result = static_cast<Dword>(mFile.tellg()) - 2;
+            if (result >= static_cast<Dword>(DOSHeader::size)) {
                 return result;
             }
         }
@@ -80,6 +215,7 @@ bool PEPreparator::loadDOSStub()
     mFile.seekg(dosStubOffset);
     mDOSStub.raw.resize(dosStubSize);
     mFile.read(&mDOSStub.raw[0], dosStubSize);
+
     if (mFile.gcount() != dosStubSize) {
         error() << "File IO error while reading DOS stub" << std::endl;
         return false;
@@ -104,6 +240,14 @@ std::ostream &PEPreparator::note()
 
 bool PEPreparator::prepare()
 {
-    return loadDOSHeader() && checkDOSHeader();
+    note() << "Loaded file size is " << mRawSize << std::endl;
+
+    return loadDOSHeader()
+        && checkDOSHeader()
+        && loadDOSStub()
+        && loadPEHeader()
+        && checkPEHeader()
+        && loadImage()
+        ;
 }
 
--- PEPreparator.hpp
+++ PEPreparator.hpp
@@ -8,11 +8,16 @@ private:
     typedef unsigned char Byte;
     typedef unsigned short Word;
     typedef unsigned int Dword;
+    typedef Dword Rva;
 
+    bool loadFromFile(int pos, int size, char *dst, std::string const &name);
     bool loadDOSHeader();
     bool checkDOSHeader();
     int findPEOffset();
     bool loadDOSStub();
+    bool loadPEHeader();
+    bool checkPEHeader();
+    bool loadImage();
 
     std::ostream &error();
     std::ostream &note();
@@ -23,28 +28,122 @@ private:
     {
     public:
         static const int size = 64;
-
     private:
-        Byte mMagicNumber[2];
-        Byte mDummy[size - sizeof(mMagicNumber) - sizeof(Dword)];
+        static const int mMagicNumberSize = 2;
 
     public:
         DOSHeader();
         bool magicNumberIsCorrect() const;
 
-        Dword peOffset;
-    };
-
-    class DOSStub
-    {
+    private:
+        Byte mMagicNumber[mMagicNumberSize];
+        Byte mDummy[size - sizeof(mMagicNumber) - sizeof(Dword)];
     public:
-        std::vector<char> raw;
+        Dword peOffset;
     };
 #pragma pack (pop)
 
-    class DOSStub;
-    class PEHeader;
-    class Image;
+    struct DOSStub
+    {
+        std::vector<char> raw;
+    };
+
+#pragma pack (push, 1)
+    struct NTFileHeader
+    {
+        static const int size = 20;
+
+        Word machine;
+        Word numofSections;
+        Dword timeDateStamp;
+        Dword pointerToSymbolTable;
+        Dword numofSymbols;
+        Word sizeofOptionalHeader;
+        Word characteristics;
+    };
+
+    struct DirectoryEntry
+    {
+        Rva rva;
+        Dword size;
+    };
+
+    struct NTOptionalHeader
+    {
+        static const int size = 224;
+        static const int numofDirectories = 16;
+
+    private:
+        Dword dummy[4];
+    public:
+        Rva entryPointRva;
+        Rva baseOfCode;
+        Rva baseOfData;
+        Rva imageBase;
+        Dword sectionAlignment;
+        Dword fileAligment;
+    private:
+        Byte dummy1[size - numofDirectories * sizeof(DirectoryEntry) - 10 * sizeof(Dword)];
+    public:
+        DirectoryEntry directories[numofDirectories];
+    };
+
+    class PEHeader
+    {
+    public:
+        PEHeader();
+        bool magicNumberIsCorrect() const;
+        int size() const;
+
+    private:
+        static const int mMagicNumberSize = 4;
+
+    private:
+        Byte mMagicNumber[mMagicNumberSize];
+    public:
+        NTFileHeader fst;
+        NTOptionalHeader snd;
+    };
+
+    struct SectionHeader
+    {
+        static const int size = 40;
+        static const int nameSize = 8;
+
+        SectionHeader();
+
+        char name[nameSize];
+        Dword virtualSize;
+        Rva rva;
+        Dword sizeofRawData;
+        Dword pointerToRawData;
+
+        Dword pointerToRelocations;
+        Dword pointerToLinenumbers;
+        Word numofRelocations;
+        Word numofLinenumbers;
+        Dword characteristics;
+    };
+#pragma pack (pop)
+
+    class Image
+    {
+    public:
+        typedef std::vector<char> Section;
+
+        void setNumofSections(int numofSections);
+        int sectionHeadersSizeInBytes() const;
+        char *rawSectionHeaders();
+        void initSectionSizes();
+        std::string nameOfSectionAt(int index) const;
+
+        Section &sectionAt(int index);
+        int rawOffsetOfSectionAt(int index) const;
+
+    private:
+        std::vector<SectionHeader> mSectionHeaders;
+        std::vector<Section> mSections;
+    };
 
     std::istream &mFile;
     std::ostream &mLog;
@@ -52,6 +151,8 @@ private:
 
     DOSHeader mDOSHeader;
     DOSStub mDOSStub;
+    PEHeader mPEHeader;
+    Image mImage;
 
 public:
     PEPreparator(std::istream &file, std::ostream &log = std::clog);
